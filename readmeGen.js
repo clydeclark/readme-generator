8d8a002f4946c6159b658aae4044ac41597547fe
Clyde Clark <clydeclark78@gmail.com>
2020-09-12 16:51:45 -0700

    initial commit

---
+++ readmeGen.js
@@ -0,0 +1,18 @@
+// require fs, inquirer
+const fs = require("fs");
+const inquirer = require("inquirer");
+
+// prompt questions
+inquirer
+    .prompt([{
+        //  {
+        //     type: "list",
+        //     name: "license",
+        //     message: "What kind of license should your project have?",
+        //     choices: ["MIT", "APACHE 2.0", "GPL 3.0", "BSD 3", "None"]
+        //   },
+
+    }])
+    .then(function (response) {
+
+    })
